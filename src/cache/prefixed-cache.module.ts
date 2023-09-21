007d98a69e507077d047e61d5d3a0726cabc53e5
L.F. Severgnini <luis@demand.io>
2023-09-21 13:46:58 -0300

    fix: re-add global annotation...

--- src/cache/prefixed-cache.module.ts
+++ src/cache/prefixed-cache.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrefixedCacheManagerService } from './prefixed-cache-manager.service';
 import { PrefixedCacheManagerFactory } from './prefixed-cache-manager.factory';
 
+@Global()
 @Module({
   imports: [ConfigModule],
   providers: [{ ...PrefixedCacheManagerFactory }],
