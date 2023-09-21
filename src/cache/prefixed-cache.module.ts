bf0c5b20d64093c7503e61df437f20a6d28bf756
L.F. Severgnini <luis@demand.io>
2023-09-21 13:44:22 -0300

    fix: adjust Prefixed Cache module metadata

--- src/cache/prefixed-cache.module.ts
+++ src/cache/prefixed-cache.module.ts
@@ -1,12 +1,11 @@
-import { Global, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrefixedCacheManagerService } from './prefixed-cache-manager.service';
 import { PrefixedCacheManagerFactory } from './prefixed-cache-manager.factory';
 
-@Global()
 @Module({
   imports: [ConfigModule],
-  providers: [PrefixedCacheManagerFactory, PrefixedCacheManagerService],
+  providers: [{ ...PrefixedCacheManagerFactory }],
   exports: [PrefixedCacheManagerFactory, PrefixedCacheManagerService],
 })
 export class PrefixedCacheModule {}
